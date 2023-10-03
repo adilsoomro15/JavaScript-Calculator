97ab359d35d5c8d3400df3c5f10edf792e1195b1
Adil Soomro <adilgulsoomro@gmail.com>
2023-10-03 13:05:18 -0400

    Fifth Commit - Added Basic JS Variables

---
+++ script.js
@@ -0,0 +1,34 @@
+let preVal = ''
+let newVal = ''
+let resultVal = ''
+let mathOperator = ''
+// Store whether or not decimal was clicked
+// (only allow one decimal per value)
+let decimalClicked = false
+// Hold values we want stored in memory
+let valMemStored = ''
+
+
+function numButPress(num) {
+
+}
+
+function mathButPress(operator) {
+
+}
+
+function equalButPress() {
+
+}
+
+function clearButPress() {
+
+}
+
+function copyButPress() {
+
+}
+
+function pasteButPress() {
+
+}
